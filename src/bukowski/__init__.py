272db4104d829cfe143da8264dde829513a7c8cd
Manabu Niseki <manabu.niseki@gmail.com>
2024-11-30 09:19:06 +0900

    feat: add --version option

--- src/bukowski/__init__.py
+++ src/bukowski/__init__.py
@@ -1,2 +1,6 @@
-def hello() -> str:
-    return "Hello from p-to-uv!"
+import importlib.metadata
+
+try:
+    __version__ = importlib.metadata.version(__name__)
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "0.0.0"
--- src/bukowski/cli.py
+++ src/bukowski/cli.py
@@ -10,6 +10,15 @@ from .main import poetry_to_uv
 app = typer.Typer()
 
 
+def version_callback(value: bool):
+    if value:
+        from . import __version__
+
+        print(__version__)  # noqa: T201
+
+        raise typer.Exit()
+
+
 @app.command()
 def convert(
     path: Annotated[Path, typer.Argument(help="path to pyproject.toml")] = Path(
@@ -23,6 +32,10 @@ def convert(
             help="Whether to overwrite the existing pyproject.toml file or not",
         ),
     ] = False,
+    _: Annotated[
+        bool | None,
+        typer.Option("--version", callback=version_callback, help="Show version"),
+    ] = None,
 ):
     poetry = Factory().create_poetry(path)
     pyproject = poetry_to_uv(poetry)
